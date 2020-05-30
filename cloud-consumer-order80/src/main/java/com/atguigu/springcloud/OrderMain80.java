e4a9c6776e3af72f55cad797214a2b4923099c82
yangyi <yy1037889792@163.com>
2020-05-30 20:49:55 +0800

    add author

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -3,6 +3,10 @@ package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * by yangyi
+ */
+
 @SpringBootApplication
 public class OrderMain80 {
     public static void main(String[] args) {
